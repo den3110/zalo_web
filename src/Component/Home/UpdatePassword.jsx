b876d2c2a77a0bd7368e46fd433bca79451f8cba
hanhtran3107 <hanhtran.mkt@gmail.com>
2023-04-25 12:01:25 +0700

    commit

--- src/Component/Home/UpdatePassword.jsx
+++ src/Component/Home/UpdatePassword.jsx
@@ -6,9 +6,10 @@ import DialogContent from "@mui/material/DialogContent";
 // import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from "@mui/material/DialogTitle";
 import Slide from "@mui/material/Slide";
-import { TextField } from "@mui/material";
+import { InputAdornment, TextField } from "@mui/material";
 import update_password from "../../api/update_password";
 import { useSnackbar } from "notistack";
+import {BiShow, BiHide } from "react-icons/bi"
 
 const Transition = React.forwardRef(function Transition(props, ref) {
   return <Slide direction="up" ref={ref} {...props} />;
@@ -28,9 +29,12 @@ export default function UpdatePassword() {
   const [oldPassword, setOldPassword] = React.useState("");
   const [newPassword, setNewPassword] = React.useState("");
   const [confirmPassword, setConfirmPassword] = React.useState("");
+  const [showPassword1, setShowPassword1]= React.useState(false)
+  const [showPassword2, setShowPassword2]= React.useState(false)
+  const [showPassword3, setShowPassword3]= React.useState(false)
 
   return (
-    <div>
+    <div style={{padding: 10}}>
       <Button variant="outlined" onClick={handleClickOpen}>
         Đổi mật khẩu
       </Button>
@@ -44,31 +48,52 @@ export default function UpdatePassword() {
         <DialogTitle>{"Đổi mật khẩu"}</DialogTitle>
         <DialogContent>
           <TextField
-            type={"password"}
+            type={showPassword1 ? "text" : "password"}
             value={oldPassword}
             onChange={(e) => setOldPassword(e.target.value)}
             label={"Nhập mật khẩu cũ"}
             style={{ width: 500, marginTop: 12}}
+            InputProps={{
+              endAdornment: <InputAdornment style={{cursor: "pointer"}} position="end">
+                {
+                  showPassword1=== false ? <BiShow onClick={()=> setShowPassword1(()=> true)} /> : <BiHide onClick={()=> setShowPassword1(()=> false)} />
+                }
+              </InputAdornment>,
+            }}
           />
           <div></div>
           <br />
           <div></div>
           <TextField
-            type={"password"}
+            type={showPassword2 ? "text" : "password"}
             value={newPassword}
             onChange={(e) => setNewPassword(e.target.value)}
             label={"Nhập mật khẩu mới"}
             style={{ width: 500, marginTop: 12}}
+            InputProps={{
+              endAdornment: <InputAdornment style={{cursor: "pointer"}} position="end">
+                {
+                  showPassword2=== false ? <BiShow onClick={()=> setShowPassword2(()=> true)} /> : <BiHide onClick={()=> setShowPassword2(()=> false)} />
+                }
+              </InputAdornment>,
+            }}
           />
           <div></div>
           <br />
           <div></div>
           <TextField
-            type={"password"}
+            type={showPassword3 ? "text" : "password"}
             value={confirmPassword}
             onChange={(e) => setConfirmPassword(e.target.value)}
             label={"Nhập lại mật khẩu mới"}
             style={{ width: 500, marginTop: 12}}
+            InputProps={{
+              endAdornment: <InputAdornment style={{cursor: "pointer"}} position="end">
+                {
+                  showPassword3=== false ? <BiShow onClick={()=> setShowPassword3(()=> true)} /> : <BiHide onClick={()=> setShowPassword3(()=> false)} />
+                }
+              </InputAdornment>,
+            }}
           />
           <div></div>
           <br />
